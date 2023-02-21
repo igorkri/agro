52c0c9e5ee2e5999d95ce25c52e32e4e5eaf1f4f
igor <igorkri26@gmail.com>
2023-02-21 16:29:09 +0200

    test widget

---
+++ frontend/widgets/FeaturedProduct.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace frontend\widgets;
+
+
+use common\models\shop\Product;
+use yii\base\Widget;
+
+class FeaturedProduct extends Widget
+{
+
+    public function init()
+    {
+        parent::init();
+
+    }
+
+    public function run()
+    {
+        $products = Product::find()->limit(20)->all();
+        return $this->render('featured-product', ['products' => $products]);
+    }
+
+
+}
