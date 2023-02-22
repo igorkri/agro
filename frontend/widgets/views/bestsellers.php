dcaa4a5f9e1e2a16f131720239ed7aabb819112a
jean <igorkri26@gmail.com>
2023-02-22 18:59:56 +0200

    колонка Найкращі товари

--- frontend/widgets/views/bestsellers.php
+++ frontend/widgets/views/bestsellers.php
@@ -138,8 +138,8 @@ use yii\helpers\Url;
                                 <?= Yii::$app->formatter->asCurrency($products[0]->price)?>
                                 </div>
                                 <div class="product-card__buttons">
-                                    <button class="btn btn-primary product-card__addtocart" type="button">Add To Cart</button>
-                                    <button class="btn btn-secondary product-card__addtocart product-card__addtocart--list" type="button">Add To Cart</button>
+                                    <button class="btn btn-primary product-card__addtocart" type="button">В Корзину</button>
+                                    <button class="btn btn-secondary product-card__addtocart product-card__addtocart--list" type="button">В Корзину</button>
                                     <button class="btn btn-light btn-svg-icon btn-svg-icon--fake-svg product-card__wishlist" type="button">
                                         <svg width="16px" height="16px">
                                             <use xlink:href="images/sprite.svg#wishlist-16"></use>
@@ -288,7 +288,7 @@ use yii\helpers\Url;
                                 <?= Yii::$app->formatter->asCurrency($product->price)?>
                                 </div>
                                 <div class="product-card__buttons">
-                                    <button class="btn btn-primary product-card__addtocart" type="button">Add To Cart</button>
+                                    <button class="btn btn-primary product-card__addtocart" type="button">В Корзину</button>
                                     <button class="btn btn-secondary product-card__addtocart product-card__addtocart--list" type="button">Add To Cart</button>
                                     <button class="btn btn-light btn-svg-icon btn-svg-icon--fake-svg product-card__wishlist" type="button">
                                         <svg width="16px" height="16px">
