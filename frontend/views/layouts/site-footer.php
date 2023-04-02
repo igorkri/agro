797ef045cf8036d640527ca0efcbb41839bc1476
Jean <mikitenko@i.ua>
2023-04-02 21:58:54 +0300

    футер*****

--- frontend/views/layouts/site-footer.php
+++ frontend/views/layouts/site-footer.php
@@ -4,125 +4,144 @@ use yii\helpers\Url;
 
 ?>
 <footer class="site__footer">
-        <div class="site-footer">
-            <div class="container">
-                <div class="site-footer__widgets">
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4">
-                            <div class="site-footer__widget footer-contacts">
-                                <h5 class="footer-contacts__title">Наші контакти</h5>
-                                <div class="footer-contacts__text">
-                                    Допоможемо підібрати надійну систему захисту та розвитку сільськогосподарським культурам.
-                                </div>
-                                <ul class="footer-contacts__contacts">
-                                    <li><i class="footer-contacts__icon fas fa-globe-americas"></i> Україна Полтава вул.Зіньківська 35, ін:36000</li>
-                                    <li><i class="footer-contacts__icon far fa-envelope"></i> nisatatyana@gmail.com</li>
-                                    <li><i class="footer-contacts__icon fas fa-mobile-alt"></i> (068) 489-43-86 </li>
-                                    <li><i class="footer-contacts__icon fas fa-mobile-alt"></i> (066) 394-18-28 </li>
-                                    <li><i class="footer-contacts__icon far fa-clock"></i> Пн-Пт 9:00 - 17:00</li>
-                                </ul>
+    <div class="site-footer">
+        <div class="container">
+            <div class="site-footer__widgets">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="site-footer__widget footer-contacts">
+                            <h5 class="footer-contacts__title">Наші контакти</h5>
+                            <div class="footer-contacts__text">
+                                Допоможемо підібрати надійну систему захисту та розвитку сільськогосподарським
+                                культурам.
                             </div>
-                        </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <div class="site-footer__widget footer-links">
-                                <h5 class="footer-links__title">Інформація</h5>
-                                <ul class="footer-links__list">
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Про нас</a></li>
-                                    <li class="footer-links__item"><a href="<?= Url::to(['/delivery/view']) ?>" class="footer-links__link">Про доставку</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Конфіденційність</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Бренди</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Контакти</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Повернення</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Карта сайту</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <div class="site-footer__widget footer-links">
-                                <h5 class="footer-links__title">Товари</h5>
-                                <ul class="footer-links__list">
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Розпродаж</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Нові надходження</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Лідери продажу</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Популярні Категорії</a></li>
-                                    <li class="footer-links__item"><a href="<?= Url::to(['/category/list']) ?>" class="footer-links__link">Каталог</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Спеціальні пропозиції</a></li>
-                                    <li class="footer-links__item"><a href="" class="footer-links__link">Статті</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-12 col-lg-4">
-                            <div class="site-footer__widget footer-newsletter">
-                                <h5 class="footer-newsletter__title">Новини</h5>
-                                <div class="footer-newsletter__text">
-                                    Підпишіться на нашу розсилку новин.
-                                </div>
-                                <form action="" class="footer-newsletter__form">
-                                    <label class="sr-only" for="footer-newsletter-address">Електронна адреса...</label>
-                                    <input type="text" class="footer-newsletter__form-input form-control" id="footer-newsletter-address" placeholder="Електронна адреса...">
-                                    <button class="footer-newsletter__form-button btn btn-primary">Підписатись</button>
-                                </form>
-                                <div class="footer-newsletter__text footer-newsletter__text--social">
-                                    Соціальні мережі
-                                </div>
-                                <!-- social-links -->
-                                <div class="social-links footer-newsletter__social-links social-links--shape--circle">
-                                    <ul class="social-links__list">
-                                        <li class="social-links__item">
-                                            <a class="social-links__link social-links__link--type--rss" href="" target="_blank">
-                                                <i class="fas fa-rss"></i>
-                                            </a>
-                                        </li>
-                                        <li class="social-links__item">
-                                            <a class="social-links__link social-links__link--type--youtube" href="" target="_blank">
-                                                <i class="fab fa-youtube"></i>
-                                            </a>
-                                        </li>
-                                        <li class="social-links__item">
-                                            <a class="social-links__link social-links__link--type--facebook" href="" target="_blank">
-                                                <i class="fab fa-facebook-f"></i>
-                                            </a>
-                                        </li>
-                                        <li class="social-links__item">
-                                            <a class="social-links__link social-links__link--type--twitter" href="" target="_blank">
-                                                <i class="fab fa-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li class="social-links__item">
-                                            <a class="social-links__link social-links__link--type--instagram" href="" target="_blank">
-                                                <i class="fab fa-instagram"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <!-- social-links / end -->
-                            </div>
+                            <ul class="footer-contacts__contacts">
+                                <li><i class="footer-contacts__icon fas fa-globe-americas"></i> Україна Полтава
+                                    вул.Зіньківська 35, ін:36000
+                                </li>
+                                <li><i class="footer-contacts__icon far fa-envelope"></i> nisatatyana@gmail.com</li>
+                                <li><i class="footer-contacts__icon fas fa-mobile-alt"></i> (068) 489-43-86</li>
+                                <li><i class="footer-contacts__icon fas fa-mobile-alt"></i> (066) 394-18-28</li>
+                                <li><i class="footer-contacts__icon far fa-clock"></i> Пн-Пт 9:00 - 17:00</li>
+                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="site-footer__bottom">
-                    <div class="site-footer__copyright">
-                        <!-- copyright -->
-                        © Copyright 2023 <a href="/" target="_blank">agropro.org.ua</a>
-                        <!-- copyright / end -->
+                    <div class="col-6 col-md-3 col-lg-2">
+                        <div class="site-footer__widget footer-links">
+                            <h5 class="footer-links__title">Інформація</h5>
+                            <ul class="footer-links__list">
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Про нас</a></li>
+                                <li class="footer-links__item"><a href="<?= Url::to(['/delivery/view']) ?>"
+                                                                  class="footer-links__link">Про доставку</a></li>
+                                <li class="footer-links__item"><a href="/"
+                                                                  class="footer-links__link">Конфіденційність</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Бренди</a></li>
+                                <li class="footer-links__item"><a href="<?= Url::to(['/contact/view']) ?>"
+                                                                  class="footer-links__link">Контакти</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Повернення</a>
+                                </li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Карта сайту</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="site-footer__payments">
-                        <img src="/images/payments.png" alt="">
+                    <div class="col-6 col-md-3 col-lg-2">
+                        <div class="site-footer__widget footer-links">
+                            <h5 class="footer-links__title">Товари</h5>
+                            <ul class="footer-links__list">
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Розпродаж</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Нові
+                                        надходження</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Лідери продажу</a>
+                                </li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Популярні
+                                        Категорії</a></li>
+                                <li class="footer-links__item"><a href="<?= Url::to(['/category/list']) ?>"
+                                                                  class="footer-links__link">Каталог</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Спеціальні
+                                        пропозиції</a></li>
+                                <li class="footer-links__item"><a href="/" class="footer-links__link">Статті</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-12 col-lg-4">
+                        <div class="site-footer__widget footer-newsletter">
+                            <h5 class="footer-newsletter__title">Новини</h5>
+                            <div class="footer-newsletter__text">
+                                Підпишіться на нашу розсилку новин.
+                            </div>
+                            <form action="" class="footer-newsletter__form">
+                                <label class="sr-only" for="footer-newsletter-address">Електронна адреса...</label>
+                                <input type="text" class="footer-newsletter__form-input form-control"
+                                       id="footer-newsletter-address" placeholder="Електронна адреса...">
+                                <button class="footer-newsletter__form-button btn btn-primary">Підписатись</button>
+                            </form>
+                            <div class="footer-newsletter__text footer-newsletter__text--social">
+                                Соціальні мережі
+                            </div>
+                            <!-- social-links -->
+                            <div class="social-links footer-newsletter__social-links social-links--shape--circle">
+                                <ul class="social-links__list">
+                                    <li class="social-links__item">
+                                        <a class="social-links__link social-links__link--type--rss" href="/"
+                                           target="_blank">
+                                            <i class="fas fa-rss"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-links__item">
+                                        <a class="social-links__link social-links__link--type--youtube" href="/"
+                                           target="_blank">
+                                            <i class="fab fa-youtube"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-links__item">
+                                        <a class="social-links__link social-links__link--type--facebook" href="/"
+                                           target="_blank">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-links__item">
+                                        <a class="social-links__link social-links__link--type--twitter" href="/"
+                                           target="_blank">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li class="social-links__item">
+                                        <a class="social-links__link social-links__link--type--instagram" href="/"
+                                           target="_blank">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- social-links / end -->
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="totop">
-                <div class="totop__body">
-                    <div class="totop__start"></div>
-                    <div class="totop__container container"></div>
-                    <div class="totop__end">
-                        <button type="button" class="totop__button">
-                            <svg width="13px" height="8px">
-                                <use xlink:href="/images/sprite.svg#arrow-rounded-up-13x8"></use>
-                            </svg>
-                        </button>
-                    </div>
+            <div class="site-footer__bottom">
+                <div class="site-footer__copyright">
+                    <!-- copyright -->
+                    © Copyright 2023 <a href="/" target="_blank">agropro.org.ua</a>
+                    <!-- copyright / end -->
+                </div>
+                <div class="site-footer__payments">
+                    <img src="/images/payments.png" alt="">
                 </div>
             </div>
         </div>
-    </footer>
+        <div class="totop">
+            <div class="totop__body">
+                <div class="totop__start"></div>
+                <div class="totop__container container"></div>
+                <div class="totop__end">
+                    <button type="button" class="totop__button">
+                        <svg width="13px" height="8px">
+                            <use xlink:href="/images/sprite.svg#arrow-rounded-up-13x8"></use>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
