2a09e8e6d2866aa5216b199dc39ebe34656d2611
jean <igorkri26@gmail.com>
2023-06-05 08:25:19 +0300

    *** front tag

--- frontend/widgets/TagCloud.php
+++ frontend/widgets/TagCloud.php
@@ -17,7 +17,14 @@ class TagCloud extends Widget
 
     public function run()
     {
+        $result = [];
         $tags = Tag::find()->all();
+        foreach ($tags as $tag) {
+            if ($tag->getProductTag($tag->id) != 0) {
+                $result[] = $tag;
+            }
+        }
+        $tags = $result;
         return $this->render('tag-cloud', ['tags' => $tags]);
 
     }
