6d720ccf1b2e511d10d0ef434ee8b81d884ce2ff
Jean <mikitenko@i.ua>
2023-06-13 00:14:09 +0300

    product *******

--- frontend/controllers/TagController.php
+++ frontend/controllers/TagController.php
@@ -4,6 +4,7 @@
 namespace frontend\controllers;
 
 use common\models\shop\Product;
+use common\models\shop\ProductProperties;
 use common\models\shop\ProductTag;
 use common\models\shop\Tag;
 use yii\base\BaseObject;
@@ -13,8 +14,8 @@ use yii\web\Controller;
 class TagController extends Controller
 {
 
-    public function actionView($id)
-    {
+    public function actionView($id) {
+
         $tag_name = Tag::find()->where(['id' => $id])->one();
         $tags = ProductTag::find()->where(['tag_id' => $id])->all();
 
