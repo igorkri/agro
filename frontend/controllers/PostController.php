4768f4e4394aa6b030289e7df67e698453ae9de2
Jean <mikitenko@i.ua>
2023-06-28 20:45:56 +0300

    Seo Posts ****

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -11,13 +11,13 @@ class PostController extends Controller {
 
     public function actionView($slug)
     {
-        $blogs = Posts::find()->limit(4)->all();
+        $blogs = Posts::find()->limit(4)->orderBy('date_public DESC')->all();
         $post = Posts::find()->where(['slug' => $slug])->one();
 
         Yii::$app->metamaster
             ->setTitle($post->seo_title)
             ->setDescription($post->seo_description)
-            ->setImage('/posts/'.$post->image)
+            ->setImage('/frontend/web/posts/'.$post->image)
             ->register(Yii::$app->getView());
 
         return $this->render('view', ['post' => $post, 'blogs' => $blogs]);
