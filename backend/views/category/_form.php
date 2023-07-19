0e90b25f0c9e6130b4b32e55be0f4e1cabd97768
Jean <mikitenko@i.ua>
2023-07-19 17:56:57 +0300

    *** Category

--- backend/views/category/_form.php
+++ backend/views/category/_form.php
@@ -201,7 +201,7 @@ use yii\helpers\Url;
                         <div class="card w-100 mt-5">
                             <div class="card-body p-5">
                                 <div class="mb-5">
-                                    <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2 class="mb-0 fs-exact-18"><?= Yii::t('app', 'Image 700x700') ?></h2></span>
+                                    <span class="sa-nav__menu-item-badge badge badge-sa-pill badge-sa-theme-cart"><h2 class="mb-0 fs-exact-18"><?= Yii::t('app', 'Image 231 x 231') ?></h2></span>
                                     </div>
                                 <div class="p-4 d-flex justify-content-center">
                                     <div class="max-w-20x">
