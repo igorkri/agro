7e15c9e104b43383a9d64c01236a18479ed06811
Jean <mikitenko@i.ua>
2023-08-08 18:56:55 +0300

    *** sort status

--- frontend/controllers/CategoryController.php
+++ frontend/controllers/CategoryController.php
@@ -6,6 +6,7 @@ use common\models\shop\Category;
 use common\models\shop\Product;
 use Yii;
 use yii\data\Pagination;
+use yii\db\Expression;
 use yii\web\Controller;
 
 
@@ -43,7 +44,14 @@ class CategoryController extends Controller
     public function actionCatalog($slug)
     {
         $category = Category::find()->where(['slug' => $slug])->one();
-        $query = Product::find()->where(['category_id' => $category->id]);
+//        $query = Product::find()->where(['category_id' => $category->id]);
+        $query = Product::find()
+            ->where(['category_id' => $category->id])
+            ->orderBy([
+                new Expression('FIELD(status_id, 1, 3, 4, 2)')
+            ]);
+
+
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 12]);
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $products_all = $query->count();
