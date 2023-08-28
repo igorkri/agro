84b38127dad1f4105b57b77b38c41e0bf69fd50a
Jean <mikitenko@i.ua>
2023-08-29 00:08:02 +0300

    Post shema

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -6,18 +6,35 @@ namespace frontend\controllers;
 use common\models\Posts;
 use Yii;
 use yii\web\Controller;
+use Spatie\SchemaOrg\Schema;
 
-class PostController extends Controller {
+class PostController extends Controller
+{
 
     public function actionView($slug)
     {
         $blogs = Posts::find()->limit(6)->orderBy('date_public DESC')->all();
         $post = Posts::find()->where(['slug' => $slug])->one();
+        $schemaPost = Schema::blogPosting()
+            ->headline($post->title)
+            ->description($post->seo_description)
+            ->datePublished($post->date_public)
+            ->review(Schema::review()
+                ->reviewRating(Schema::rating()->ratingValue(4)->bestRating(5))
+                ->author(Schema::person()->name('Tatyana Khalimon')))
+            ->image(Yii::$app->request->hostInfo . '/frontend/web/posts/' . $post->webp_image)
+            ->articleBody($post->description)
+            ->mainEntityOfPage(Yii::$app->request->hostInfo . '/post/' . $post->slug)
+            ->url(Yii::$app->request->hostInfo . '/post/' . $post->slug)
+            ->aggregateRating(Schema::aggregateRating()
+                ->ratingValue(4.3)
+                ->reviewCount(27));
+        Yii::$app->params['schema'] = $schemaPost->toScript();
 
         Yii::$app->metamaster
             ->setTitle($post->seo_title)
             ->setDescription($post->seo_description)
-            ->setImage('/frontend/web/posts/'.$post->image)
+            ->setImage(Yii::$app->request->hostInfo . '/frontend/web/posts/' . $post->webp_image)
             ->register(Yii::$app->getView());
 
         return $this->render('view', ['post' => $post, 'blogs' => $blogs]);
