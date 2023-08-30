684eb317a57a9222d340120723d1638464c0c85d
Jean <mikitenko@i.ua>
2023-08-30 22:21:31 +0300

    ***

--- frontend/views/layouts/shop.php
+++ frontend/views/layouts/shop.php
@@ -1,8 +1,11 @@
 <?php
 /** @var yii\web\View $this */
+
 /** @var string $content */
 
 use frontend\assets\AppAsset;
+use frontend\widgets\SiteFooter;
+use frontend\widgets\SiteHeader;
 use yii\helpers\Html;
 
 AppAsset::register($this);
@@ -10,59 +13,34 @@ AppAsset::register($this);
 ?>
 
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" dir="ltr">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!--    <link type="image/ico" href="/images/favicon.ico" rel="icon">-->
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?= Yii::$app->params['schema'] ?? '' ?>
-    <?php $this->head() ?>
-</head>
-
-<body>
-<?php $this->beginBody() ?>
-<!-- site -->
-<div class="site">
-
-    <!-- mobile site__header -->
-    <?php if(\Yii::$app->devicedetect->isMobile()): ?>
-          <?= $this->render('mobile-site-header')?>
-    <?php endif; ?>
-    <!-- mobile site__header / end -->
-
-    <!-- desktop site__header -->
-    <?php echo \frontend\widgets\SiteHeader::widget() ?>
-    <!--  desktop site__header / end -->
-
-    <?= $content ?>
-
-    <!-- site__footer -->
-    <?php echo \frontend\widgets\SiteFooter::widget() ?>
-    <!-- site__footer / end -->
-    
-</div>
-<!-- site / end -->
-
-<!-- quickview-modal -->
-<div id="quickview-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
-        <div class="modal-content"></div>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>" dir="ltr">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?= Yii::$app->params['schema'] ?? '' ?>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <div class="site">
+        <?php if (\Yii::$app->devicedetect->isMobile()): ?>
+            <?= $this->render('mobile-site-header') ?>
+        <?php endif; ?>
+        <?php echo SiteHeader::widget() ?>
+        <?= $content ?>
+        <?php echo SiteFooter::widget() ?>
     </div>
-</div>
-<!-- quickview-modal / end -->
-
-<!-- mobilemenu -->
-       <?=$this->render('mobilemenu')?>
-<!-- mobilemenu / end -->
-
-<!-- photoswipe -->
-       <?=$this->render('photoswipe')?>
-<!-- photoswipe / end -->
-
-<?php $this->endBody() ?>
-</body>
-</html>
+    <div id="quickview-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-content"></div>
+        </div>
+    </div>
+    <?= $this->render('mobilemenu') ?>
+    <?= $this->render('photoswipe') ?>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
 <?php $this->endPage() ?>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,6 +2,7 @@
 
 /** @var yii\web\View $this */
 
+use common\models\shop\ActivePages;
 use frontend\widgets\FeaturedProduct;
 use frontend\widgets\PopularCategories;
 use frontend\widgets\Bestsellers;
@@ -15,67 +16,27 @@ use frontend\widgets\BlockSlideshow;
 use frontend\widgets\BlockBrands;
 use frontend\widgets\BlockBanner;
 
-\common\models\shop\ActivePages::setActiveUser();
+ActivePages::setActiveUser();
 
-//$this->title = Yii::$app->name;
 ?>
-<!-- site__body -->
 <div class="site__body">
-    
-    <!-- .block-slideshow -->
     <?php echo BlockSlideshow::widget() ?>
-    <!-- .block-slideshow / end -->
-    
-    <!-- .block-features -->
     <?php echo BlockFeatures::widget() ?>
-    <!-- .block-features / end -->
-   
-    <!-- .block-products-carousel -->
     <?php echo FeaturedProduct::widget() ?>
-    <!-- .block-products-carousel / end -->
-    
-    <!-- .block-banner -->
     <?php echo BlockBanner::widget() ?>
-    <!-- .block-banner / end -->
-    
-    <!-- .block-products -->
     <?php echo Bestsellers::widget() ?>
-    <!-- .block-products / end -->
-   
-    <!-- .block-categories -->
     <?php echo PopularCategories::widget() ?>
-    <!-- .block-categories / end -->
-   
-    <!-- .block-products-carousel -->
     <?php echo ProductsCarousel::widget() ?>
-    <!-- .block-products-carousel / end -->
-    
-    <!-- .block-posts -->
     <?php echo BlockPosts::widget() ?>
-    <!-- .block-posts / end -->
-   
-    <!-- .block-brands -->
     <?php echo BlockBrands::widget() ?>
-    <!-- .block-brands / end -->
-   
-    <!-- .block-product-columns -->
     <div class="block block-product-columns d-lg-block d-none">
         <div class="container">
             <div class="row">
-   <!-- .block-product-columns Top Rated Products-->
    <?php echo ColumnsTopRated::widget() ?>
-   <!-- .block-product-columns Top Rated Products /end -->
-
-   <!-- .block-product-columns Special Offers -->           
    <?php echo ColumnsSpecialOffers::widget() ?>
-   <!-- .block-product-columns Special Offers /end -->
-
-   <!-- .block-product-columns Bestsellers -->            
    <?php echo ColumnsBestsellers::widget() ?>
-   <!-- .block-product-columns Bestsellers /end -->
             </div>
         </div>
     </div>
-    <!-- .block-product-columns / end -->
 </div>
-<!-- site__body / end -->
+
