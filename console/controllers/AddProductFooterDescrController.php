966b611c15b6c31950fe735ae7168e8a3f6eb76d
Jean <mikitenko@i.ua>
2023-09-14 11:55:29 +0300

    add product footer descr

---
+++ console/controllers/AddProductFooterDescrController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace console\controllers;
+
+
+use common\models\shop\Product;
+
+class AddProductFooterDescrController extends \yii\console\Controller
+{
+    public function actionFooterDescr()
+    {
+        $descr = '<p>---------------------------
+</p>
+<p><strong>Увага!!!</strong>  Для безпечного використання препарату (*name_product*) та досягнення максимальної ефективності його дії, слід строго дотримуватися інструкцій виробника та правил техніки безпеки при обробці хімічних речовин.
+</p>
+<p>Інтернет-магазин <a href="http://agro/">AgroPro</a> пропонує одні з найвигідніших цін на (*name_product*). Ви можете замовити необхідний препарат на нашому веб-сайті, і наші менеджери оперативно оброблять та доставлять ваше замовлення.
+</p>
+<p>Наші модератори дуже уважно перевіряють інформацію, перед тим як публікувати її на сайті. Однак, на жаль, дані про товар можуть змінюватися виробником без попередження, тому інтернет-магазин <a href="http://agro/">AgroPro</a> не несе відповідальності за точність опису, і наявна помилка не може служити підставою для повернення товару.
+</p>';
+
+        $products = Product::find()->all();
+        $categories = [11, 20, 18, 26];
+        foreach ($products as $product) {
+            if (!in_array($product->category_id, $categories) && $product->footer_description === null) {
+                $product->footer_description = $descr;
+                $product->save();
+                echo "\t" . $product->id . " Footer описание добавлено! \t" . $product->category_id . "\n";
+            }else{
+                echo "\t" . $product->id . " Footer описание НЕ добавлено! \t" . $product->category_id . "\n";
+            }
+        }
+    }
+}
