25229933aa7dd4c390daf9b32afbbfab050e2ff6
Jean <mikitenko@i.ua>
2024-02-17 23:28:13 +0200

    category url

--- frontend/views/category/list.php
+++ frontend/views/category/list.php
@@ -34,40 +34,53 @@ ActivePages::setActiveUser();
             <div class="col-12">
                 <div class="block">
                     <div class="products-view">
-                         <div class="products-view__list products-list" data-layout="grid-4-full" data-with-features="false" data-mobile-grid-columns="2">
+                        <div class="products-view__list products-list" data-layout="grid-4-full"
+                             data-with-features="false" data-mobile-grid-columns="2">
                             <div class="products-list__body">
                                 <?php foreach ($categories as $category): ?>
-                                <div class="products-list__item">
-                                    <div class="product-card ">
-                                        <div class="product-card__image product-image">
-                                            <?php if(empty($category->products)): ?>
-                                            <a href="<?=Url::to(['category/children', 'slug' => $category->slug])?>" class="product-image__body">
-                                            <?php else: ?>
-                                            <a href="<?=Url::to(['category/catalog', 'slug' => $category->slug])?>" class="product-image__body">
-                                            <?php endif; ?>
-                                                <img class="product-image__img" src="/category/<?= $category->file ?>" width="231" height="231" alt="<?= $category->name ?>">
-                                            </a>
-                                        </div>
-                                        <div class="product-card__info">
-                                            <div class="product-card__name">
-                                                <a href="<?=Url::to(['category/children', 'slug' => $category->slug])?>"><?= $category->name ?></a>
+                                    <div class="products-list__item">
+                                        <div class="product-card ">
+                                            <div class="product-card__image product-image">
+                                                <?php if (empty($category->products)): ?>
+                                                <a href="<?= Url::to(['category/children', 'slug' => $category->slug]) ?>"
+                                                   class="product-image__body">
+                                                    <?php else: ?>
+                                                    <a href="<?= Url::to(['category/catalog', 'slug' => $category->slug]) ?>"
+                                                       class="product-image__body">
+                                                        <?php endif; ?>
+                                                        <img class="product-image__img"
+                                                             src="/category/<?= $category->file ?>" width="231"
+                                                             height="231" alt="<?= $category->name ?>">
+                                                    </a>
+                                            </div>
+                                            <div class="product-card__info">
+                                                <div class="product-card__name">
+                                                    <?php if (empty($category->products)): ?>
+                                                        <a href="<?= Url::to(['category/children', 'slug' => $category->slug]) ?>"><?= $category->name ?></a>
+                                                    <?php else: ?>
+                                                        <a href="<?= Url::to(['category/catalog', 'slug' => $category->slug]) ?>"><?= $category->name ?></a>
+                                                    <?php endif; ?>
+                                                </div>
+                                            </div>
+                                            <div class="product-card__actions">
                                             </div>
                                         </div>
-                                        <div class="product-card__actions">
-                                        </div>
                                     </div>
-                                </div>
                                 <?php endforeach; ?>
                             </div>
-                             <div class="spec__disclaimer">
-                                 Знаходьте найкращі Захистні Засоби Рослин, Добрива, Посівний Матеріал та Боротьбу з Гризунами в
-                                 AgroPro – вашому надійному інтернет-магазині для сільського господарства та сільськогосподарських потреб.
-                                 Оптимізуйте врожаї та збільшуйте врожайність за допомогою наших високоякісних товарів. Доставка та консультації
-                                 експертів - завжди на вашому боці.
-                                 Придбайте Фуміганти та Родентициди для боротьби з шкідниками, а також найкращі Добрива та Посівний Матеріал
-                                 для успішного вирощування рослин.
-                                 Оберіть AgroPro для найкращого відповідного сільськогосподарського рішення!
-                             </div>
+                            <div class="spec__disclaimer">
+                                Знаходьте найкращі Захистні Засоби Рослин, Добрива, Посівний Матеріал та Боротьбу з
+                                Гризунами в
+                                AgroPro – вашому надійному інтернет-магазині для сільського господарства та
+                                сільськогосподарських потреб.
+                                Оптимізуйте врожаї та збільшуйте врожайність за допомогою наших високоякісних товарів.
+                                Доставка та консультації
+                                експертів - завжди на вашому боці.
+                                Придбайте Фуміганти та Родентициди для боротьби з шкідниками, а також найкращі Добрива
+                                та Посівний Матеріал
+                                для успішного вирощування рослин.
+                                Оберіть AgroPro для найкращого відповідного сільськогосподарського рішення!
+                            </div>
                         </div>
                     </div>
                 </div>
