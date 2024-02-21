21b3fb413711d804a52e63b6c6ce5dcd1f064cf0
Jean <mikitenko@i.ua>
2024-02-21 14:28:49 +0200

    *

--- console/controllers/ProductPopertiesController.php
+++ console/controllers/ProductPopertiesController.php
@@ -2,6 +2,7 @@
 
 namespace console\controllers;
 
+use common\models\shop\Category;
 use common\models\shop\Product;
 use common\models\shop\ProductProperties;
 use yii\console\Controller;
@@ -102,5 +103,50 @@ class ProductPopertiesController extends Controller
         $newArray = array_unique($newArray);
 
         echo "\t" . print_r($newArray) . "\n";
+    }
+
+    /**
+     * Добавление новых характеристик
+     */
+    public function actionAddNewProperties()
+    {
+        $sort = 10;
+        $catId = 5;
+
+        $nameProperty = 'група:';
+        $valueProperty = Category::find()->select('name')->where(['id' => $catId])->one();
+
+        $productsId = ProductProperties::find()
+            ->select('product_id')
+            ->where(['category_id' => $catId])
+            ->distinct()
+            ->column();
+        $i = 1;
+        foreach ($productsId as $item) {
+            $nameProduct = Product::find()->select('name')->where(['id' => $item])->one();
+            $productProperty = ProductProperties::find()->select('properties')->where(['product_id' => $item])->all();
+            $res = 0;
+            foreach ($productProperty as $property) {
+                if ($property->properties == $nameProperty) {
+                    $res = 1;
+                }
+            }
+            if ($res === 0) {
+                $model = new ProductProperties();
+                $model->product_id = $item;
+                $model->properties = $nameProperty;
+                $model->value = $valueProperty->name;
+                $model->sort = $sort;
+                $model->category_id = $catId;
+                if ($model->save()) {
+                    echo $i . " Новое свойство " . $nameProperty . " сохранено: Для продукта " . $nameProduct->name . "\n";
+                    $i++;
+                }else{
+                    echo "ERROR: Для продукта " . $nameProduct->name . "\n";
+                }
+            }else{
+                echo "### Свойство " . $nameProperty . " уже существует: Для продукта " . $nameProduct->name . "\n";
+            }
+        }
     }
 }
