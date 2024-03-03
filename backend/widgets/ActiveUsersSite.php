c1925e2b15edbf1ecdfd7018975036807895593d
Jean <mikitenko@i.ua>
2024-03-03 17:16:43 +0200

    graf visit user js **

--- backend/widgets/ActiveUsersSite.php
+++ backend/widgets/ActiveUsersSite.php
@@ -22,12 +22,22 @@ class ActiveUsersSite extends Widget
             ->all();
 
         $carts = [];
+        $ukrainian_months = [
+            'Jan' => 'Січ', 'Feb' => 'Лют', 'Mar' => 'Бер',
+            'Apr' => 'Кві', 'May' => 'Тра', 'Jun' => 'Чер',
+            'Jul' => 'Лип', 'Aug' => 'Сер', 'Sep' => 'Вер',
+            'Oct' => 'Жов', 'Nov' => 'Лис', 'Dec' => 'Гру'
+        ];
+
         foreach ($users as $user) {
+            $month_name = date('M', $user->date_visit);
+            $ukrainian_month_name = $ukrainian_months[$month_name] ?? '';
             $carts[] = [
-                "label" => date('M', $user->date_visit),
+                "label" => $ukrainian_month_name,
                 "value" => 1,
             ];
         }
+
         $resultArray = [];
         foreach ($carts as $item) {
             $label = $item['label'];
@@ -42,9 +52,9 @@ class ActiveUsersSite extends Widget
                 ];
             }
         }
+
         $resultArray = array_values($resultArray);
 
         return $this->render('active-users-site', ['resultArray' => json_encode($resultArray)]);
     }
-
 }
