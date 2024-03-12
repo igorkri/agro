09fcee0fd3d6e6bd7aab83a445e35a49f45a622f
Jean <mikitenko@i.ua>
2024-03-12 19:24:10 +0200

    min js css

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -19,17 +19,24 @@ class AppAsset extends AssetBundle
 //        'https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i',
         '/vendor/bootstrap/css/bootstrap.min.css?v=' . PROJECT_VERSION,
         '/vendor/owl-carousel/assets/owl.carousel.min.css?v=' . PROJECT_VERSION,
+
         '/vendor/photoswipe/photoswipe.css?v=' . PROJECT_VERSION,
+//        '/vendor/photoswipe/photoswipe.min.css?v=' . PROJECT_VERSION,
+
         '/vendor/photoswipe/default-skin/default-skin.css?v=' . PROJECT_VERSION,
+//        '/vendor/photoswipe/default-skin/default-skin.min.css?v=' . PROJECT_VERSION,
+
         '/vendor/select2/css/select2.min.css?v=' . PROJECT_VERSION,
         '/css/style.css?v=' . PROJECT_VERSION,
+//        '/css/style.min.css?v=' . PROJECT_VERSION,
+
 //        '/css/jquery.toast.css?v=' . PROJECT_VERSION,
         '/vendor/fontawesome/css/all.min.css?v=' . PROJECT_VERSION,
-        '/fonts/stroyka/stroyka.css?v=' . PROJECT_VERSION,
 
+        '/fonts/stroyka/stroyka.css?v=' . PROJECT_VERSION,
     ];
     public $js = [
-//          '/vendor/jquery/jquery.min.js?v=' . PROJECT_VERSION,
+//         '/vendor/jquery/jquery.min.js?v=' . PROJECT_VERSION,
         // 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.jsx',
         '/vendor/bootstrap/js/bootstrap.bundle.min.js?v=' . PROJECT_VERSION,
         '/vendor/owl-carousel/owl.carousel.min.js?v=' . PROJECT_VERSION,
@@ -40,9 +47,13 @@ class AppAsset extends AssetBundle
 //        '/js/jquery.toast.js?v=' . PROJECT_VERSION,
         '/js/number.js?v=' . PROJECT_VERSION,
         '/js/header.js?v=' . PROJECT_VERSION,
+        '/js/main.js?v=' . PROJECT_VERSION,
+//        '/js/number.min.js?v=' . PROJECT_VERSION,
+//        '/js/header.min.js?v=' . PROJECT_VERSION,
+//        '/js/main.min.js?v=' . PROJECT_VERSION,
 //        '/js/ModalRemote.js?v=' . PROJECT_VERSION,
 //        '/js/ajaxcrud.js?v=' . PROJECT_VERSION,
-        '/js/main.js?v=' . PROJECT_VERSION,
+
 //        '/vendor/svg4everybody/svg4everybody.min.js?v=' . PROJECT_VERSION,
     ];
     public $depends = [
