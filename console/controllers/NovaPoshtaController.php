0f98554bbf1615e4e41418fea501b62f931056d6
Jean <mikitenko@i.ua>
2024-03-24 23:47:54 +0200

    *

--- console/controllers/NovaPoshtaController.php
+++ console/controllers/NovaPoshtaController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace console\controllers;
 
 use common\models\NpAreas;
@@ -16,8 +17,8 @@ class NovaPoshtaController extends Controller
      * Получить области
      */
 
-    public function actionAreas(){
-        // npAreas
+    public function actionAreas()
+    {
         $np = new NovaPoshtaApi2(
             self::KEY,
             'ua', // Язык возвращаемых данных: ru (default) | ua | en
@@ -26,37 +27,36 @@ class NovaPoshtaController extends Controller
         );
 
         $areas = $np->getAreas();
-        $i = 1; $n = 1;
-        foreach ($areas['data'] as $area){
+        $i = 1;
+        $n = 1;
+        foreach ($areas['data'] as $area) {
             $model = NpAreas::find()->where(['Ref' => $area['Ref']])->one();
-            if(!$model){
+            if (!$model) {
                 $new_model = new NpAreas();
-                $new_model->ref             = $area['Ref'];
-                $new_model->areasCenter     = $area['AreasCenter'];
-                $new_model->description     = $area['Description'];
-                if($new_model->save()){
-                    echo "| " . $i . " | " . $new_model->description . "\n";
+                $new_model->ref = $area['Ref'];
+                $new_model->areasCenter = $area['AreasCenter'];
+                $new_model->description = $area['Description'];
+                if ($new_model->save()) {
+                    echo "|+ " . $i . " | " . $new_model->description . "\n";
                     echo "\r+---+------------------------------------------------------------------------------+\n";
-                }else{
+                } else {
                     print_r($new_model->errors);
                 }
                 $i++;
-            }else{
-                echo "| " . $n . " | " . $model->description . " Сущесвует\n";
+            } else {
+                echo "|- " . $n . " | " . $model->description . " Сущесвует\n";
                 echo "\r+---+------------------------------------------------------------------------------+\n";
                 $n++;
             }
-
         }
-
     }
 
     /**
      * Получить города у областей
      */
 
-    public function actionCities(){
-        // npCities
+    public function actionCities()
+    {
         $np = new NovaPoshtaApi2(
             self::KEY,
             'ru', // Язык возвращаемых данных: ru (default) | ua | en
@@ -65,30 +65,29 @@ class NovaPoshtaController extends Controller
         );
 
         $cities = $np->getCities();
-//        print_r($cities); die;
-        $i = 1; $n = 1;
-        foreach ($cities['data'] as $array){
+        $i = 1;
+        $n = 1;
+        foreach ($cities['data'] as $array) {
             $model = NpCity::find()->where(['Ref' => $array['Ref']])->one();
-            if(!$model) {
+            if (!$model) {
                 $city = new NpCity();
                 $city->description = $array['Description'];
                 $city->ref = $array['Ref'];
                 $city->area = $array['Area'];
                 $city->cityID = $array['CityID'];
                 if ($city->save()) {
-                    echo "| " . $i . " | " . $city->description . "\n";
+                    echo "|+ " . $i . " | " . $city->description . "\n";
                     echo "\r+---+------------------------------------------------------------------------------+\n";
                 } else {
                     print_r($city->errors);
                 }
                 $i++;
-            }else{
-                echo "| " . $n . " | " . $model->description . " Сущесвует\n";
+            } else {
+                echo "|- " . $n . " | " . $model->description . " Сущесвует\n";
                 echo "\r+---+------------------------------------------------------------------------------+\n";
                 $n++;
             }
         }
-
     }
 
     /**
@@ -96,32 +95,25 @@ class NovaPoshtaController extends Controller
      */
     public function actionWarehouses()
     {
-
         $np = new NovaPoshtaApi2(
             self::KEY,
             'ru', // Язык возвращаемых данных: ru (default) | ua | en
             FALSE, // При ошибке в запросе выбрасывать Exception: FALSE (default) | TRUE
             'file_get_content' // Используемый механизм запроса: curl (defalut) | file_get_content
-        // 'curl' // Используемый механизм запроса: curl (defalut) | file_get_content
         );
 
-
         $cities = NpCity::find()->select('ref, area, cityID')->asArray()->all();
-
-        if($cities){
+        if ($cities) {
+            $i = 1;
+            $n = 1;
             foreach ($cities as $city) {
                 $cityRef = $city['ref'];
-
-//                print_r($cityRef); die;
-
                 $warehouses = $np->getWarehouses($cityRef);
-
-                if(!empty($warehouses)){
-                    if(isset($warehouses['data'])){
-                        $i = 1; $n = 1;
-                        foreach ($warehouses['data'] as $warehouse ){
+                if (!empty($warehouses)) {
+                    if (isset($warehouses['data'])) {
+                        foreach ($warehouses['data'] as $warehouse) {
                             $model = NpWarehouses::find()->where(['ref' => $warehouse['Ref']])->one();
-                            if(!$model) {
+                            if (!$model) {
                                 $warehousesdb = new NpWarehouses();
                                 $warehousesdb->description = $warehouse['Description'];
                                 $warehousesdb->ref = $warehouse['Ref'];
@@ -129,21 +121,20 @@ class NovaPoshtaController extends Controller
                                 $warehousesdb->cityRef = $warehouse['CityRef'];
                                 $warehousesdb->shortAddress = $warehouse['ShortAddress'];
                                 if ($warehousesdb->save(false)) {
-                                    echo "| " . $i . " | " . $warehousesdb->description . "\n";
-                                    echo "\r+---+------------------------------------------------------------------------------+\n";
+                                    echo "|# " . $i . " | " . $warehousesdb->description . "\n";
+                                    echo "\r+-----+----------------------------------------------------------------------------------------+\n";
                                     $i++;
                                 } else {
                                     print_r($warehousesdb->errors);
                                 }
-                            }else{
-                                echo "| " . $n . " | " . $model->description . " Сущесвует\n";
-                                echo "\r+---+------------------------------------------------------------------------------+\n";
+                            } else {
+                                echo "|- " . $n . " | " . $model->description . " Сущесвует\n";
+                                echo "\r+-----+----------------------------------------------------------------------------------------+\n";
                                 $n++;
                             }
                         }
                     }
-
-                }else{
+                } else {
                     echo PHP_EOL . 'error warehouses' . PHP_EOL;
                     print_r($warehouses);
                     echo PHP_EOL . 'error cityRef' . PHP_EOL;
@@ -156,10 +147,11 @@ class NovaPoshtaController extends Controller
     /**
      * Очистить талицу городов Новая Почта
      */
-    public function actionTruncateCities(){
+    public function actionTruncateCities()
+    {
 
         $rm_cities = Yii::$app->db->createCommand()->truncateTable('npCity')->execute();
-        if($rm_cities){
+        if ($rm_cities) {
             echo "\n очищено таблицу городов {`npCities`} НП \n";
         }
     }
@@ -167,20 +159,23 @@ class NovaPoshtaController extends Controller
     /**
      * Очистить талицу отделений Новая Почта
      */
-    public function actionTruncateWarehouses(){
+    public function actionTruncateWarehouses()
+    {
 
         $rm_warehouses = Yii::$app->db->createCommand()->truncateTable('npWarehouses')->execute();
-        if($rm_warehouses){
+        if ($rm_warehouses) {
             echo "\n очищено таблицу отделения {`npWarehouses`} НП \n";
         }
     }
+
     /**
      * Очистить талицу областей Новая Почта
      */
-    public function actionTruncateAreas(){
+    public function actionTruncateAreas()
+    {
 
         $rm_warehouses = Yii::$app->db->createCommand()->truncateTable('npAreas')->execute();
-        if($rm_warehouses){
+        if ($rm_warehouses) {
             echo "\n очищено таблицу области {`npAreas`} НП \n";
         }
     }
