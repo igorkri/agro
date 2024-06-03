ef40dc67ce18cf684df017f0a1bf67f686f601f8
Jean <mikitenko@i.ua>
2024-06-03 15:24:53 +0300

    оптимизация css ***

---
+++ frontend/assets/CategoryAuxiliaryPageAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+class CategoryAuxiliaryPageAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+
+        '/css/view-options.css?v=' . PROJECT_VERSION,
+
+    ];
+    public $js = [
+
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+//        'yii\bootstrap5\BootstrapAsset',
+    ];
+    public $cssOptions = [
+        'type' => 'text/css',
+    ];
+}
