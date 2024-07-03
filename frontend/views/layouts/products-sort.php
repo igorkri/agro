af2ac9ad1f31c684e16bbe8002cacf83a57a7fb9
Jean <mikitenko@i.ua>
2024-07-03 20:02:03 +0300

    products sort translate

--- frontend/views/layouts/products-sort.php
+++ frontend/views/layouts/products-sort.php
@@ -4,65 +4,67 @@ use yii\helpers\Html;
 use yii\web\View;
 
 ?>
-<div class="view-options__layout">
-    <div class="layout-switcher">
-        <div class="layout-switcher__list">
-            <button data-layout="grid-4-full" data-with-features="false"
-                    title="Плитка"
-                    type="button" class="layout-switcher__button">
-                <svg width="16px" height="16px">
-                    <use xlink:href="/images/sprite.svg#layout-grid-16x16"></use>
-                </svg>
-            </button>
-            <button data-layout="list" data-with-features="false" title="Список"
-                    type="button" class="layout-switcher__button">
-                <svg width="16px" height="16px">
-                    <use xlink:href="/images/sprite.svg#layout-list-16x16"></use>
-                </svg>
-            </button>
+    <div class="view-options__layout">
+        <div class="layout-switcher">
+            <div class="layout-switcher__list">
+                <button data-layout="grid-4-full" data-with-features="false"
+                        title="Плитка"
+                        type="button" class="layout-switcher__button">
+                    <svg width="16px" height="16px">
+                        <use xlink:href="/images/sprite.svg#layout-grid-16x16"></use>
+                    </svg>
+                </button>
+                <button data-layout="list" data-with-features="false" title="Список"
+                        type="button" class="layout-switcher__button">
+                    <svg width="16px" height="16px">
+                        <use xlink:href="/images/sprite.svg#layout-list-16x16"></use>
+                    </svg>
+                </button>
+            </div>
         </div>
     </div>
-</div>
-<div class="view-options__legend"><?= Yii::t('app', 'Показано') ?> <span class="count-products"><?= count($products) ?></span> <?= Yii::t('app', 'товарів з') ?>
-    <span class="count-products"><?= $products_all ?></span></div>
-<div class="view-options__divider"></div>
-<div class="view-options__control">
-    <label for="sort"><?= Yii::t('app', 'Сортувати') ?></label>
-    <div>
-        <?php echo Html::dropDownList('sort', Yii::$app->session->get('sort'), [
-            '' => Yii::t('app','Наявність'),
-            'price_lowest' => Yii::t('app','Ціна Дешевші'),
-            'price_highest' => Yii::t('app','Ціна Дорожчі'),
-            'name_a' => Yii::t('app','Назва A-я'),
-            'name_z' => Yii::t('app','Назва Я-а'),
-        ], ['class' => 'form-control form-control-sm count-products', 'id' => 'sort-form']);
-        ?>
+    <div class="view-options__legend"><?= Yii::t('app', 'Показано') ?>
+        <span class="count-products"><?= count($products) ?></span>
+        <?= Yii::t('app', 'товарів з') ?>
+        <span class="count-products"><?= $products_all ?></span></div>
+    <div class="view-options__divider"></div>
+    <div class="view-options__control">
+        <label for="sort"><?= Yii::t('app', 'Сортувати') ?></label>
+        <div>
+            <?php echo Html::dropDownList('sort', Yii::$app->session->get('sort'), [
+                '' => Yii::t('app', 'Наявність'),
+                'price_lowest' => Yii::t('app', 'Ціна Дешевші'),
+                'price_highest' => Yii::t('app', 'Ціна Дорожчі'),
+                'name_a' => Yii::t('app', 'Назва A-я'),
+                'name_z' => Yii::t('app', 'Назва Я-а'),
+            ], ['class' => 'form-control form-control-sm count-products', 'id' => 'sort-form']);
+            ?>
+        </div>
     </div>
-</div>
-<div class="view-options__control">
-    <label for="count"><?= Yii::t('app', 'Показати') ?></label>
-    <div>
-        <?php
-        echo Html::dropDownList('count', Yii::$app->session->get('count'), [
-            '4' => '4',
-            '8' => '8',
-            '12' => '12',
-            '24' => '24',
-            '32' => '32',
-        ], ['class' => 'form-control form-control-sm count-products', 'id' => 'count-form']);
-        ?>
+    <div class="view-options__control">
+        <label for="count"><?= Yii::t('app', 'Показати') ?></label>
+        <div>
+            <?php
+            echo Html::dropDownList('count', Yii::$app->session->get('count'), [
+                '4' => '4',
+                '8' => '8',
+                '12' => '12',
+                '24' => '24',
+                '32' => '32',
+            ], ['class' => 'form-control form-control-sm count-products', 'id' => 'count-form']);
+            ?>
+        </div>
     </div>
-</div>
-<style>
-    .count-products {
-        color: white;
-        font-weight: bold;
-        font-size: 17px;
-        background-color: rgba(94, 180, 52, 0.78);
-        padding: 0 5px;
-        border-radius: 3px;
-    }
-</style>
+    <style>
+        .count-products {
+            color: white;
+            font-weight: bold;
+            font-size: 17px;
+            background-color: rgba(94, 180, 52, 0.78);
+            padding: 0 5px;
+            border-radius: 3px;
+        }
+    </style>
 <?php
 $script = <<< JS
 
