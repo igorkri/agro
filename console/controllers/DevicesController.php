cadb105b6c7b7a28683cd5741cb6d156cbc89a72
Jean <mikitenko@i.ua>
2024-10-24 20:42:47 +0300

    ***

--- console/controllers/DevicesController.php
+++ console/controllers/DevicesController.php
@@ -27,15 +27,74 @@ class DevicesController extends Controller
 
 
         $agents = ActivePages::find()->all();
-        foreach ($agents as $agent){
-        if ($agent->other === null){
-            if (strpos($agent->user_agent, $device) !== false){
+        foreach ($agents as $agent) {
+            if ($agent->other === null) {
+                if (strpos($agent->user_agent, $device) !== false) {
 //                $agent->other = 'mobile';
-                $agent->other = 'desktop';
-                $agent->save();
+                    $agent->other = 'desktop';
+                    $agent->save();
+                }
             }
-        }
 
         }
+    }
+
+    public function actionDetectRobotsIp()
+    {
+        $ips = ActivePages::find()->select('ip_user')->distinct()->column();
+        $countIps = count($ips);
+        $item = $countIps - 1000;
+        usort($ips, function ($a, $b) {
+            return strlen($a) - strlen($b);
+        });
+        foreach ($ips as $ip) {
+            if ($countIps <= $item) {
+                $url = "http://ip-api.com/json/{$ip}";
+                $response = $this->getIpInfoFromApi($url);
+                // Проверяем успешность ответа
+                if ($response && $response['status'] === 'success') {
+                    $org = $response['org'];
+                    if (isset($response['org']) && strpos(strtolower($response['org']), 'google') !== false) {
+                        // Output in red for Google robots
+                        echo "$countIps\t\033[0;31m$ip\tRobot Google.\t$org\033[0m\n"; // Reset color after the message
+                    } else {
+                        // Output in green for non-robot
+                        echo "$countIps\t\033[0;32m$ip\tNot Robot.\t$org\033[0m\n"; // Reset color after the message
+                    }
+                } else {
+                    echo "$ip: Error retrieving information.\n";
+                }
+                sleep(2);
+            }
+            $countIps--;
+        }
+    }
+
+    /**
+     * Метод для отправки запроса к API и получения данных
+     */
+    private function getIpInfoFromApi($url)
+    {
+        // Инициализация curl
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Возвращаем результат в виде строки
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Таймаут запроса
+
+        // Выполняем запрос
+        $output = curl_exec($ch);
+
+        // Проверка на наличие ошибок
+        if (curl_errno($ch)) {
+            curl_close($ch);
+            return null;
+        }
+
+        // Закрываем соединение
+        curl_close($ch);
+
+        // Возвращаем декодированные данные в формате массива
+        return json_decode($output, true);
     }
 }
