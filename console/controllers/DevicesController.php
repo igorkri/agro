3e43baef5c7c37487e43f1b410cb9285272ca9ec
Jean <mikitenko@i.ua>
2024-10-30 22:15:45 +0200

    поиск ip robots ***

--- console/controllers/DevicesController.php
+++ console/controllers/DevicesController.php
@@ -45,11 +45,11 @@ class DevicesController extends Controller
         $ips = ActivePages::find()
             ->select(['ip_user', 'date_visit'])
             ->orderBy('date_visit DESC')
-            ->limit(100)
+            ->limit(10)
             ->asArray()
             ->all();
 
-        $ips = array_map(function($item) {
+        $ips = array_map(function ($item) {
             unset($item['date_visit']);
             return $item;
         }, $ips);
@@ -61,6 +61,7 @@ class DevicesController extends Controller
         $robotProviders = ['google', 'hetzner', 'huawei', 'digitalocean', 'amazon', 'microsoft', 'oracle', 'alibaba'];
         $filePath = Yii::getAlias('@frontend/runtime/robots_control.txt');
         $i = 1;
+        $ipDelete = [];
 
         foreach ($ips as $ip) {
             $ip = $ip['ip_user'];
@@ -70,13 +71,13 @@ class DevicesController extends Controller
             if ($response && $response['status'] === 'success') {
                 $org = strtolower($response['isp']);
                 $isRobot = false;
-
                 foreach ($robotProviders as $provider) {
                     if (str_contains($org, $provider)) {
                         $isRobot = true;
                         echo "$countIps\t\033[0;31m$ip\tRobot\t{$response['isp']}\033[0m\n";
                         $logEntry = "'$ip',\n";
                         file_put_contents($filePath, $logEntry, FILE_APPEND);
+                        $ipDelete[] = $ip;
                         $i++;
                         break;
                     }
@@ -92,6 +93,9 @@ class DevicesController extends Controller
             sleep(2);
             $countIps--;
         }
+        if ($ipDelete) {
+            $this->getIpDelete($ipDelete);
+        }
     }
 
     /**
@@ -99,27 +103,37 @@ class DevicesController extends Controller
      */
     private function getIpInfoFromApi($url)
     {
-        // Инициализация curl
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Возвращаем результат в виде строки
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Таймаут запроса
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
-        // Выполняем запрос
         $output = curl_exec($ch);
 
-        // Проверка на наличие ошибок
         if (curl_errno($ch)) {
             curl_close($ch);
             return null;
         }
 
-        // Закрываем соединение
         curl_close($ch);
 
-        // Возвращаем декодированные данные в формате массива
         return json_decode($output, true);
+    }
+
+    /**
+     * Метод для удаления Ip из базы данных
+     */
+    private function getIpDelete($ipDelete)
+    {
+        $ips = ActivePages::find()->where(['ip_user' => $ipDelete])->all();
+        foreach ($ips as $ip) {
+            if ($ip->delete()) {
+                echo "\t\033[0;31m{$ip->ip_user}\tУдален из базы данных\033[0m\n";
+            } else {
+                echo "\t\033[0;33m{$ip->ip_user}\tНе удалось удалить из базы данных\033[0m\n";
+            }
+        }
     }
 
 }
