55078ffeeab50eea14704546fc91c9ed1cb3b2dd
Jean <mikitenko@i.ua>
2024-11-23 20:35:22 +0200

    лимит городов

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -8,6 +8,7 @@ use yii\console\Controller;
 use common\models\NpCity;
 use common\models\NpWarehouses;
 use LisDev\Delivery\NovaPoshtaApi2;
+use yii\db\Expression;
 
 class CronController extends Controller
 {
@@ -86,7 +87,7 @@ class CronController extends Controller
 
             if (!$isRobot) {
                 $this->outputIpInfo($ip, $response, $countIps, false);
-            }else{
+            } else {
                 $this->outputIpInfo($ip, $response, $countIps, true);
             }
         } else {
@@ -211,53 +212,50 @@ class CronController extends Controller
             'file_get_content' // Используемый механизм запроса: curl (default) | file_get_content
         );
 
-        $cities = NpCity::find()->select('ref')->column();
+        $cities = NpCity::find()
+            ->select('ref')
+            ->orderBy(new Expression('RAND()')) // Для MySQL; для PostgreSQL используйте 'RANDOM()'
+            ->limit(100)
+            ->column();
 
         if ($cities) {
             $i = 1;
             $n = 1;
-            $j = 1;
-
-            shuffle($cities);
-
             foreach ($cities as $city) {
                 $nameCity = NpCity::find()->where(['ref' => $city])->one();
                 echo "\t" . "|#----->> " . '' . " | " . $nameCity->description . "\n";
-                if ($j >= 1) {
-                    $warehouses = $np->getWarehouses($city);
-                    if (!empty($warehouses)) {
-                        if ($warehouses['data'] != null) {
-                            foreach ($warehouses['data'] as $warehouse) {
-                                $model = NpWarehouses::find()->where(['ref' => $warehouse['Ref']])->one();
-                                if (!$model) {
-                                    $warehouses_db = new NpWarehouses();
-                                    $warehouses_db->description = $warehouse['Description'];
-                                    $warehouses_db->ref = $warehouse['Ref'];
-                                    $warehouses_db->Number = $warehouse['Number'];
-                                    $warehouses_db->cityRef = $warehouse['CityRef'];
-                                    $warehouses_db->shortAddress = $warehouse['ShortAddress'];
-                                    if ($warehouses_db->save(false)) {
-                                        echo "\t" . "|# " . $i . " | " . $warehouses_db->description . "\n";
-                                        echo "\r+--------------------------------------------------------------------------------------------------------+\n";
-                                        $i++;
-                                    } else {
-                                        print_r($warehouses_db->errors);
-                                    }
+                $warehouses = $np->getWarehouses($city);
+                if (!empty($warehouses)) {
+                    if ($warehouses['data'] != null) {
+                        foreach ($warehouses['data'] as $warehouse) {
+                            $model = NpWarehouses::find()->where(['ref' => $warehouse['Ref']])->one();
+                            if (!$model) {
+                                $warehouses_db = new NpWarehouses();
+                                $warehouses_db->description = $warehouse['Description'];
+                                $warehouses_db->ref = $warehouse['Ref'];
+                                $warehouses_db->Number = $warehouse['Number'];
+                                $warehouses_db->cityRef = $warehouse['CityRef'];
+                                $warehouses_db->shortAddress = $warehouse['ShortAddress'];
+                                if ($warehouses_db->save(false)) {
+                                    echo "\t" . "|# " . $i . " | " . $warehouses_db->description . "\n";
+                                    echo "\r+--------------------------------------------------------------------------------------------------------+\n";
+                                    $i++;
                                 } else {
-                                    // echo "\t" . "|- " . $n . " | " . $model->description . " Сущесвует\n";
-                                    // echo "\r+--------------------------------------------------------------------------------------------------------+\n";
-                                    // $n++;
+                                    print_r($warehouses_db->errors);
                                 }
+                            } else {
+                                echo "\t" . "|- " . $n . " | " . $model->description . " Сущесвует\n";
+                                echo "\r+--------------------------------------------------------------------------------------------------------+\n";
+                                $n++;
                             }
                         }
-                    } else {
-                        echo PHP_EOL . 'error warehouses' . PHP_EOL;
-                        print_r($warehouses);
-                        echo PHP_EOL . 'error city' . PHP_EOL;
-                        print_r($city);
                     }
+                } else {
+                    echo PHP_EOL . 'error warehouses' . PHP_EOL;
+                    print_r($warehouses);
+                    echo PHP_EOL . 'error city' . PHP_EOL;
+                    print_r($city);
                 }
-                $j++;
             }
         }
     }
