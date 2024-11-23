0e4585b9a3226b74942fc41c54513c4c5796467d
Jean <mikitenko@i.ua>
2024-11-23 13:21:24 +0200

    *

--- backend/views/tag/index.php
+++ backend/views/tag/index.php
@@ -72,11 +72,8 @@ $this->title = Yii::t('app', 'Tags');
                                             aria-expanded="false"
                                             aria-label="More"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="3" height="13"
-                                             fill="currentColor">
-                                            <path
-                                                    d="M1.5,8C0.7,8,0,7.3,0,6.5S0.7,5,1.5,5S3,5.7,3,6.5S2.3,8,1.5,8z M1.5,3C0.7,3,0,2.3,0,1.5S0.7,0,1.5,0 S3,0.7,3,1.5S2.3,3,1.5,3z M1.5,10C2.3,10,3,10.7,3,11.5S2.3,13,1.5,13S0,12.3,0,11.5S0.7,10,1.5,10z"
-                                            ></path>
+                                        <svg width="3" height="13" style="display: unset">
+                                            <use xlink:href="/admin/images/sprite.svg#drop-menu"/>
                                         </svg>
                                     </button>
                                     <ul class="dropdown-menu dropdown-menu-end"
--- backend/widgets/views/tasks-admin.php
+++ backend/widgets/views/tasks-admin.php
@@ -2,18 +2,18 @@
 <button
     class="sa-toolbar__button"
     type="button"
-    id="dropdownMenuButton2"
+    id="dropdownMenuButtonTasks"
     data-bs-toggle="dropdown"
     data-bs-reference="parent"
     data-bs-offset="0,1"
     aria-expanded="false"
 >
-    <svg width="20px" height="20px" style="display: unset; font-size: larger; color: rgba(255,90,90)">
+    <svg width="20px" height="20px" style="display: unset; font-size: larger; color: rgb(255,88,88)">
         <use xlink:href="/admin/images/sprite.svg#tasks-admin"/>
     </svg>
     <span class="sa-toolbar__button-indicator"><?= count($tasks) ?></span>
 </button>
-<div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="dropdownMenuButton2">
+<div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="dropdownMenuButtonTasks">
     <div class="sa-notifications">
         <div class="sa-notifications__header">
             <div class="sa-notifications__header-title">Повідомлення</div>
@@ -26,16 +26,8 @@
                     <div class="sa-notifications__item-icon">
                         <div class="sa-symbol sa-symbol--shape--rounded sa-symbol--style--primary">
                             <div class="sa-symbol__icon">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="1em"
-                                    height="1em"
-                                    viewBox="0 0 16 16"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        d="M14.5,15h-1c-0.8,0-1.5-0.7-1.5-1.5v-8C12,4.7,12.7,4,13.5,4h1C15.3,4,16,4.7,16,5.5v8C16,14.3,15.3,15,14.5,15z M8.5,15h-1C6.7,15,6,14.3,6,13.5v-11C6,1.7,6.7,1,7.5,1h1C9.3,1,10,1.7,10,2.5v11C10,14.3,9.3,15,8.5,15z M2.5,15h-1C0.7,15,0,14.3,0,13.5v-5C0,7.7,0.7,7,1.5,7h1C3.3,7,4,7.7,4,8.5v5C4,14.3,3.3,15,2.5,15z"
-                                    ></path>
+                                <svg width="16px" height="16px" style="display: unset">
+                                    <use xlink:href="/admin/images/sprite.svg#item-menu"/>
                                 </svg>
                             </div>
                         </div>
