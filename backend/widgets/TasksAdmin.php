14b068dd7ce94150f9c505df055c4bcee0dcc6d4
Jean <mikitenko@i.ua>
2024-12-21 15:21:51 +0200

    tasks admin

--- backend/widgets/TasksAdmin.php
+++ backend/widgets/TasksAdmin.php
@@ -2,9 +2,11 @@
 
 namespace backend\widgets;
 
+use common\models\Posts;
 use common\models\shop\Product;
 use common\models\shop\Tag;
 use kartik\base\Widget;
+use Yii;
 
 class TasksAdmin extends Widget
 {
@@ -17,25 +19,65 @@ class TasksAdmin extends Widget
 
     public function run()
     {
+        $tasks[] = $this->countProducts();
+        $tasks[] = $this->countTags();
+        $tasks[] = $this->countPosts();
 
+        return $this->render('tasks-admin', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Количество продуктов которых нужно добавить
+     */
+    protected function countProducts()
+    {
+        $addCountProducts = 1000;
         $countsProduct = Product::find()->count();
         $addProducts = [
-            'title' => 'Добавить товар 1000шт.',
-            'descr' => 'Осталось ' . 1000 - $countsProduct,
+            'svg' => 'products',
+            'title' => 'Добавить товар ' . $addCountProducts . ' шт.',
+            'descr' => 'Осталось ' . '<span style="font-weight: bold;">' . $addCountProducts - $countsProduct . '</span>' . ' шт.',
         ];
+        return $addProducts;
+    }
 
+    /**
+     * Теги без описания
+     */
+    protected function countTags()
+    {
         $countsTag = Tag::find()->count();
         $countsNotDescr = Tag::find()->where(['description' => ''])->orWhere(['description' => null])->count();
         $addTagDescr = [
+            'svg' => 'tags',
             'title' => 'Добавить описания тегам',
-            'descr' => 'Осталось ' . $countsNotDescr . ' из ' . $countsTag,
+            'descr' => 'Осталось ' . '<span style="font-weight: bold;">' . $countsNotDescr . '</span>' . ' из ' . '<span style="font-weight: bold;">' . $countsTag . '</span>',
         ];
+        return $addTagDescr;
+    }
 
-        $tasks[] = $addProducts;
-        $tasks[] = $addTagDescr;
+    /**
+     * Количество статей которые нужно добавить
+     */
+    protected function countPosts()
+    {
+        $currentTimestamp = Yii::$app->formatter->asTimestamp('now');
+        $timeStamp = Posts::find()
+            ->select('date_public')
+            ->orderBy(['date_public' => SORT_DESC])
+            ->scalar();
 
-        return $this->render('tasks-admin', [
-            'tasks' => $tasks,
-        ]);
+        $secondsDifference = abs($currentTimestamp - $timeStamp);
+        $weeks = floor($secondsDifference / (7 * 24 * 60 * 60));
+
+        $formattedDate = Yii::$app->formatter->asDate($timeStamp, 'short');
+
+        $addPostDescr = [
+            'svg' => 'posts',
+            'title' => 'Добавлять статью каждую неделю ',
+            'descr' => 'C ' . '<span style="font-weight: bold;">' . $formattedDate . '</span>' . ' добавить ' . '<span style="font-weight: bold;">' . $weeks . '</span>' . ' статьи',
+
+        ];
+        return $addPostDescr;
     }
 }
--- backend/widgets/views/tasks-admin.php
+++ backend/widgets/views/tasks-admin.php
@@ -1,12 +1,16 @@
-<?php ?>
+<?php
+
+/** @var array $tasks */
+
+?>
 <button
-    class="sa-toolbar__button"
-    type="button"
-    id="dropdownMenuButtonTasks"
-    data-bs-toggle="dropdown"
-    data-bs-reference="parent"
-    data-bs-offset="0,1"
-    aria-expanded="false"
+        class="sa-toolbar__button"
+        type="button"
+        id="dropdownMenuButtonTasks"
+        data-bs-toggle="dropdown"
+        data-bs-reference="parent"
+        data-bs-offset="0,1"
+        aria-expanded="false"
 >
     <svg width="20px" height="20px" style="display: unset; font-size: larger; color: rgb(255,88,88)">
         <use xlink:href="/admin/images/sprite.svg#tasks-admin"/>
@@ -17,34 +21,32 @@
     <div class="sa-notifications">
         <div class="sa-notifications__header">
             <div class="sa-notifications__header-title">Повідомлення</div>
-            <a class="sa-notifications__header-action" href="/">До задач...</a>
+            <!--            <a class="sa-notifications__header-action" href="/">До задач...</a>-->
         </div>
         <ul class="sa-notifications__list">
             <?php foreach ($tasks as $task): ?>
-            <li class="sa-notifications__item">
-                <a href="/#" class="sa-notifications__item-button">
-                    <div class="sa-notifications__item-icon">
-                        <div class="sa-symbol sa-symbol--shape--rounded sa-symbol--style--primary">
-                            <div class="sa-symbol__icon">
-                                <svg width="16px" height="16px" style="display: unset">
-                                    <use xlink:href="/admin/images/sprite.svg#item-menu"/>
-                                </svg>
+                <li class="sa-notifications__item">
+                    <a href="#" class="sa-notifications__item-button">
+                        <div class="sa-notifications__item-icon">
+                            <div class="sa-symbol sa-symbol--shape--rounded sa-symbol--style--primary">
+                                <div class="sa-symbol__icon">
+                                    <svg width="16px" height="16px" style="display: unset">
+                                        <use xlink:href="/admin/images/sprite.svg#<?= $task['svg'] ?>"/>
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="sa-notifications__item-body">
-                        <div class="sa-notifications__item-title sa-notifications__item-title--nowrap">
-                            <?= $task['title'] ?>
+                        <div class="sa-notifications__item-body">
+                            <div class="sa-notifications__item-title sa-notifications__item-title--nowrap">
+                                <?= $task['title'] ?>
+                            </div>
+                            <div class="sa-notifications__item-subtitle sa-notifications__item-subtitle--nowrap">
+                                <?= $task['descr'] ?>
+                            </div>
                         </div>
-                        <div class="sa-notifications__item-subtitle sa-notifications__item-subtitle--nowrap">
-                            <?= $task['descr'] ?>
-                        </div>
-                    </div>
-                </a>
-            </li>
+                    </a>
+                </li>
             <?php endforeach; ?>
         </ul>
-        <div class="sa-notifications__footer"><a class="sa-notifications__footer-action"
-                                                 href="/">See all 15 notifications</a></div>
     </div>
 </div>
